1e52018d4681b8f5fed6e70eb1f307fad1357e83
Vadim-Rafalskiy <vadim.rafalskiy.zaknafer@gmail.com>
2022-09-06 16:13:19 +0300

    fix-modal-locations+js

--- src/js/modal.js
+++ src/js/modal.js
@@ -13,3 +13,37 @@
     refs.modal.classList.toggle("is-hidden");
   }
 })();
+
+
+(() => {
+  const refs = {
+    openModalBtn: document.querySelector("[data-modal-location-open]"),
+    closeModalBtn: document.querySelector("[data-modal-location-close]"),
+    modal: document.querySelector("[data-modal-location]"),
+  };
+
+  refs.openModalBtn.addEventListener("click", toggleModal);
+  refs.closeModalBtn.addEventListener("click", toggleModal);
+
+  function toggleModal() {
+    document.body.classList.toggle("modal-open");
+    refs.modal.classList.toggle("is-hidden");
+  }
+})();
+
+
+(() => {
+  const refs = {
+    openModalBtn: document.querySelector("[data-modal-franchise-open]"),
+    closeModalBtn: document.querySelector("[data-modal-franchise-close]"),
+    modal: document.querySelector("[data-modal-franchise]"),
+  };
+
+  refs.openModalBtn.addEventListener("click", toggleModal);
+  refs.closeModalBtn.addEventListener("click", toggleModal);
+
+  function toggleModal() {
+    document.body.classList.toggle("modal-open");
+    refs.modal.classList.toggle("is-hidden");
+  }
+})();
